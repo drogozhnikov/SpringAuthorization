e62623870ae4a70f849673c21975f5323cd39cf0
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-06-12 14:12:28 +0300

    Refactor: lazy

--- src/main/resources/db/migration/V2__insert_test_values.sql
+++ src/main/resources/db/migration/V2__insert_test_values.sql
@@ -1,5 +1,5 @@
 SET search_path TO auth;
 
 insert into users(username, email, password, role, status, created, updated)
-values ('user', 'user_email', '$2a$12$D1.ICUpw.7hYZnfml9LL4OHg6SImjzyd8ZIEWl/1tiY3WtQsFWRI6', 'USER', 'ACTIVE',
+values ('master', 'user_email@gmail.ru', '$2a$12$ogK61gygrGtruI7ix02ym.WRCzLzWiCxcpMv0Lou7weEDWq9ziA9m', 'ADMIN', 'ACTIVE',
         current_timestamp, current_timestamp);
